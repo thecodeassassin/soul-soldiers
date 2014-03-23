a0415428f2b7e74489995810e2b43bdc6dbd83d9
Stephen Hoogendijk <s.hoogendijk@leaseweb.com>
2014-03-23 20:28:23 +0100

    Added embed image feature for emails

--- app/config/services/view.php
+++ app/config/services/view.php
@@ -1,4 +1,5 @@
 <?php
+use Phalcon\DI;
 use Phalcon\Mvc\View,
     Soul\Translate,
     Phalcon\Mvc\View\Engine\Volt as VoltEngine;
@@ -7,34 +8,59 @@ use Phalcon\Mvc\View,
 /**
  * Setting up the view component
  */
-$di->set('view', function() use ($config) {
+$di->set('view', function() use ($config, $di) {
 
-    $view = new View();
+        $view = new View();
 
-    $view->setViewsDir($config->application->libraryDir.'Soul/View/');
+        $view->setViewsDir($config->application->libraryDir.'Soul/View/');
 
-    $view->registerEngines(array(
-        '.volt' => function($view, $di) use ($config) {
+        $view->registerEngines([
+                '.volt' => function ($view, $di) use ($config) {
 
-            $volt = new VoltEngine($view, $di);
+                        $volt = new VoltEngine($view, $di);
 
-            $volt->setOptions(array(
-                'compiledPath' => $config->application->cacheDir,
-                'compiledSeparator' => '_',
-                'compileAlways' => true
-            ));
+                        $volt->setOptions([
+                                'compiledPath' => $config->application->cacheDir,
+                                'compiledSeparator' => '_',
+                                'compileAlways' => true
+                            ]);
 
-            $volt->getCompiler()->addFunction(
-                't',
-                function($key) {
-                    return Translate::translate($key);
-                }
-            );
+                        $compiler = $volt->getCompiler();
 
-            return $volt;
-        },
-        '.phtml' => 'Phalcon\Mvc\View\Engine\Php' // Generate Template files uses PHP itself as the template engine
-    ));
+                        $compiler->addFunction(
+                            't',
+                            function ($key) {
+                                return Translate::translate($key);
+                            }
+                        );
 
-    return $view;
-}, true);
+                        $compiler->addFunction(
+                            'embed_image',
+                            function ($args, $params) use ($compiler, $di) {
+
+                                $image =  str_replace("'", "", $compiler->expression($params[0]['expr']));
+
+                                $fullPath = sprintf('%s/../public/%s', APPLICATION_PATH, $image);
+
+                                if (!is_readable($fullPath)) {
+                                    return '""';
+                                }
+
+                                $imageBase64 = base64_encode(file_get_contents($fullPath));
+                                $alt = '';
+
+                                if (isset($params[1])) {
+                                    $alt = $compiler->expression($params[1]['expr']);
+                                }
+
+                                return '"<img src=\'data:image/png;base64,'.$imageBase64.'\' alt='.$alt.'>"';
+                            }
+                        );
+
+                        return $volt;
+                    },
+                '.phtml' => 'Phalcon\Mvc\View\Engine\Php' // Generate Template files uses PHP itself as the template engine
+            ]);
+
+        return $view;
+    }, true);
