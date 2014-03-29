6efa8fcf918f0192d8d5556d5f6d5cdcc165c397
Stephen Hoogendijk <s.hoogendijk@leaseweb.com>
2014-03-29 20:05:42 +0100

    added contact form logic

--- app/src/Soul/Controller/ContactController.php
+++ app/src/Soul/Controller/ContactController.php
@@ -14,10 +14,41 @@ use Soul\Form\ContactForm;
  */
 class ContactController extends Base
 {
+    /**
+     *
+     */
     public function indexAction()
     {
         $contactForm = new ContactForm();
+        $submitted = false;
 
+        try {
+            if ($this->request->isPost()) {
+                $email = $this->request->getPost('email', 'email');
+                $realname = $this->request->getPost('realName', 'string');
+                $emailContent = $this->request->getPost('content', 'string');
+
+                // if the form is invalid, show the messages to the user
+                if ($contactForm->isValid($this->request->getPost()) == false) {
+                    $this->flashMessages($contactForm->getMessages(), 'error');
+
+                } else {
+                    $submitted = true;
+
+                    // if the form is valid, inform the adminEmail
+                    $this->getMail()->send([
+
+                    ])
+
+                }
+
+            }
+        } catch (AuthException $e) {
+            $this->flash->error($e->getMessage());
+        }
+
+
+        $this->view->submitted = $submitted;
         $this->view->form = $contactForm;
     }
 }
