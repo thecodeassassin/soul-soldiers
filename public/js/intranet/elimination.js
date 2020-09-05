9efcca213d69c092fe036a9e9b3095c9695e18b7
TheCodeAssassin <hoogendijk09@gmail.com>
2020-09-05 15:08:48 +0000

    tournament bracket change div #2

--- public/js/intranet/elimination.js
+++ public/js/intranet/elimination.js
@@ -1,8 +1,19 @@
-$(function (){
+$(function () {
     var bracketOptions = {
-            init: __BRACKET_DATA
-            //skipSecondaryFinal: true
+        init: __BRACKET_DATA,
+        decorator: function () {
+            jqBracket = $('.jQBracket');
+
+            if (jqBracket.find('.finals').length > 0) {
+                height = jqBracket.find('.finals').height();
+            } else {
+                height = jqBracket.find('.bracket').height();
+            }
+
+            jqBracket.css('min-height', height);
         },
+        //skipSecondaryFinal: true
+    },
         jqBracket,
         height = 0;
 
@@ -12,28 +23,20 @@ $(function (){
 
     $('#bracket').bracket(bracketOptions);
 
-    $('.team').find('.label').each(function() {
-       var originalFunction,
-           parentElem = $(this).parent(),
-           title = $(this).html();
-       parentElem.attr('title', title);
+    $('.team').find('.label').each(function () {
+        var originalFunction,
+            parentElem = $(this).parent(),
+            title = $(this).html();
+        parentElem.attr('title', title);
 
         if (title != '--' && title != __BYE_STR) {
             parentElem.tooltip({
                 trigger: 'manual'
             });
-            parentElem.hover(function() {
+            parentElem.hover(function () {
                 $(this).tooltip('toggle')
             });
         }
     });
-    jqBracket = $('.jQBracket');
 
-    if (jqBracket.find('.finals').length > 0) {
-        height = jqBracket.find('.finals').height();
-    } else {
-        height = jqBracket.find('.bracket').height();
-    }
-
-    jqBracket.css('min-height', height);
 });
